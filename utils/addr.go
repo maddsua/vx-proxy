9307ece17de687a9face394a101e14e78a2e0f3d
maddsua <maddsuadev@pm.me>
2025-08-02 02:55:25 +0200

    update utils

--- utils/addr.go
+++ utils/addr.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"errors"
 	"net"
+	"strconv"
+	"strings"
 )
 
 func GetLocalDialAddrTCP(addr net.Addr) *net.TCPAddr {
@@ -18,4 +21,43 @@ func GetLocalDialAddrTCP(addr net.Addr) *net.TCPAddr {
 	return &net.TCPAddr{
 		IP: tcpAddr.IP,
 	}
+}
+
+type Range struct {
+	Begin int
+	End   int
+}
+
+func ParseRange(token string) (*Range, error) {
+
+	if token == "" {
+		return nil, errors.New("empty token")
+	}
+
+	before, after, has := strings.Cut(token, "-")
+	if !has {
+
+		val, err := strconv.Atoi(token)
+		if err != nil {
+			return nil, err
+		}
+
+		return &Range{Begin: val, End: val}, nil
+	}
+
+	begin, err := strconv.Atoi(strings.TrimSpace(before))
+	if err != nil {
+		return nil, err
+	}
+
+	end, err := strconv.Atoi(strings.TrimSpace(after))
+	if err != nil {
+		return nil, err
+	}
+
+	if end <= begin {
+		return nil, errors.New("invalid range")
+	}
+
+	return &Range{Begin: begin, End: end}, nil
 }
