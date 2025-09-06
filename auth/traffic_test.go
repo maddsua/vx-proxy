08c65bcf07f510555230137f697fa6cf04351996
maddsua <maddsuadev@pm.me>
2025-09-06 15:26:09 +0200

    Add test cases

--- auth/traffic_test.go
+++ auth/traffic_test.go
@@ -26,24 +26,31 @@ func TestTraffic_1(t *testing.T) {
 			Delta:     4_000,
 			Bandwidth: 300_000,
 		},
+		{
+			ID:        4,
+			Delta:     4_000,
+			Bandwidth: 32_000,
+		},
 	}
 
 	auth.RecalculateBandwidth(entries, bandwidth)
 
 	var expectBandwidth = func(entry auth.TrafficState, expect int) {
 		if entry.Bandwidth != expect {
-			t.Fatal("unexpected entry bandwidth: expected:", expect, "got:", entry.Bandwidth)
+			t.Fatalf("unexpected entry %d bandwidth: expected: %d, got: %d", entry.ID, expect, entry.Bandwidth)
 		}
 	}
 
 	for _, item := range entries {
 		switch item.ID {
 		case 1:
-			expectBandwidth(item, 860_000)
+			expectBandwidth(item, 580_000)
 		case 2:
 			expectBandwidth(item, 8000)
 		case 3:
 			expectBandwidth(item, 32000)
+		case 4:
+			expectBandwidth(item, 312_000)
 		default:
 			t.Fatal("unexpected entry id:", item.ID)
 		}
